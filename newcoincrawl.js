0b265942259c33dbc8d66ceba2f7c33be914ce4e
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2018-06-29 16:21:17 +0800

    Update newcoincrawl.js

--- newcoincrawl.js
+++ newcoincrawl.js
@@ -10,12 +10,17 @@ var c = new Crawler({
             var $ = res.$;
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
+            $("[id^='msg_4']").each(function(i, e) {
+	if(!/ICO|POS|AIRDROP/i.test($(e).text())) {
+	        console.log($(e).text());
+  	        console.log($(e).find('a').attr('href'));
+	}
+	      });
         }
+       console.log("task over");
         done();
     }
 });
 
 // Queue just one URL, with default callback
-c.queue('https://bitcointalk.org/index.php?board=159.0');
-//c.queue(['https://bitcointalk.org/index.php?board=159.0','https://bitcointalk.org/index.php?board=159.40','https://bitcointalk.org/index.php?board=159.80','https://bitcointalk.org/index.php?board=159.120']);
+c.queue(['https://bitcointalk.org/index.php?board=159.0','https://bitcointalk.org/index.php?board=159.40','https://bitcointalk.org/index.php?board=159.80','https://bitcointalk.org/index.php?board=159.120']);
