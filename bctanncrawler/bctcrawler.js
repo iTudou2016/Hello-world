94684fdd2da22f43100c6ecdf77e1bf3979e2c49
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2018-07-06 17:04:15 +0800

    Update bctcrawler.js

--- bctanncrawler/bctcrawler.js
+++ bctanncrawler/bctcrawler.js
@@ -22,29 +22,30 @@ app.post('/', function (req, res) {
 var server = app.listen(8080, function () {
 var host = server.address().address;
 var port = server.address().port;
-
   console.log('Bitcointalk crawler listening at http://%s:%s', host, port);
 });
-
 
 function fetchannData(svrres) {
 // Queue just one URL, with default callback
 var bctannData = [];
+var task = [];
 var c = new Crawler({
-    maxConnections : 10,
+    //maxConnections : 10,
+    rateLimit: 500,
     // This will be called for each crawled page
     callback : function (error, res, done) {
         if(error){
             console.log(error);
         }else{
             var $ = res.$;
+            process.stdout.write(".");
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server
             $("[id^='msg_4']").each(function(i, e) {
 	var ann_msgID = $(e).attr('id').replace("msg_", "");
 	var ann_title = $(e).text();
 	var ann_href = $(e).find('a').attr('href');
-	if(ann_title.search(/ANN/)>-1&&(/POW/i.test(ann_title)||!/ICO|POS|AIRDROP|WHITELIST|SALE/i.test(ann_title))&&Number(ann_href.slice(40, -2))>4556000 ) {
+	if(ann_title.search(/ANN/)>-1&&(/POW/i.test(ann_title)||!/ICO|POS|AIRDROP|WHITELIST|SALE/i.test(ann_title))&&Number(ann_href.slice(40, -2))>4600000 ) {
 	         // 向数组插入数据
 	         bctannData.push({
 		ann_msgID: ann_msgID + "//" + ann_href.slice(40, -2),
@@ -57,10 +58,15 @@ var c = new Crawler({
         done();
     }
 });
-console.log(new Date().toLocaleTimeString() + ": Crawler work started.");
-c.queue(['https://bitcointalk.org/index.php?board=159.0','https://bitcointalk.org/index.php?board=159.40','https://bitcointalk.org/index.php?board=159.80','https://bitcointalk.org/index.php?board=159.120','https://bitcointalk.org/index.php?board=159.160','https://bitcointalk.org/index.php?board=159.200']);
+process.stdout.write(new Date().toLocaleTimeString() + ": Crawler work starting.");
+  for (var i=0; i<20; i++)
+  {
+       task.push('https://bitcointalk.org/index.php?board=159.' + i*40);
+  }
+c.queue(task);
 c.on('drain',function(){
     // 异步数据处理
+   process.stdout.write("\n");
    console.log(new Date().toLocaleTimeString() + ": Crawler work done. " + bctannData.length + " links crawled!");
    svrres.render('index', {title: 'BctANN list', message: bctannData});
 });
